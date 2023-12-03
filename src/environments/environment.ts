65b14c38dfbd546ef4e3010448ba27602b125cb1
Cleverson Ray Sucalit <cleversonsucalit@gmail.com>
2023-12-04 02:39:27 +0800

    firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    firebase : {
+        apiKey: "AIzaSyBBpAH2NTjxNFTAISiE1-2YBs_HzFBbLYc",
+        authDomain: "appsdev-80d9f.firebaseapp.com",
+        projectId: "appsdev-80d9f",
+        storageBucket: "appsdev-80d9f.appspot.com",
+        messagingSenderId: "805621516796",
+        appId: "1:805621516796:web:7e7ea46803a464dea40a79"
+    }
+
+};
