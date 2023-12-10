d17e904db087933b1f5ec299fa7d1005d71a9699
Cleverson Ray Sucalit <cleversonsucalit@gmail.com>
2023-12-11 04:26:25 +0800

    cyber signup and signin update

--- src/app/signin/signin.component.ts
+++ src/app/signin/signin.component.ts
@@ -1,8 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../auth.service';
 import { Auth } from '@angular/fire/auth';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-signin',
@@ -10,22 +8,4 @@ import { Router } from '@angular/router';
   styleUrls: ['./signin.component.css']
 })
 export class SigninComponent {
-  email = '';
-  password = '';
-
-  constructor(private afAuth: AngularFireAuth, private router: Router) {}
-
-  signin(): void {
-    const trimmedEmail = this.email.trim();
-    console.log('Trimmed Email:', trimmedEmail);
-  
-    this.afAuth.signInWithEmailAndPassword(trimmedEmail, this.password)
-      .then((userCredential) => {
-        console.log('User signed in:', userCredential.user);
-        this.router.navigate(['/dashboard']);
-      })
-      .catch((error) => {
-        console.log('Sign-in error:', error.message);
-      });
-  }  
- }
+}
