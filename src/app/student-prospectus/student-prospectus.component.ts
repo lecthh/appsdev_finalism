7671b1be42cf47452c09e73023c8d42bd2f0bf09
lecthh <dev.lecth@gmail.com>
2024-01-05 12:04:52 +0800

    enrolledsubj

--- src/app/student-prospectus/student-prospectus.component.ts
+++ src/app/student-prospectus/student-prospectus.component.ts
@@ -3,17 +3,17 @@ import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Router } from '@angular/router';
 import { AngularFirestore } from '@angular/fire/compat/firestore';
 import { Observable } from 'rxjs';
-import { catchError, map } from 'rxjs/operators';
+import { catchError, map,switchMap } from 'rxjs/operators';
 import { AuthService } from '../auth.service';
+import { forkJoin } from 'rxjs';
 
-interface CollegeData {
-  CollegeName: string;
+interface Subject {
+  id: string;
+  offerCode: string;
+  descriptiveTitle: string;
+  subjectNumber: string;
+  units: number;
 }
-
-interface ProgramData {
-  ProgName: string;
-}
-
 
 @Component({
   selector: 'app-student-prospectus',
@@ -22,71 +22,39 @@ interface ProgramData {
 })
 
 export class StudentProspectusComponent implements OnInit {
-
-  college = '';
-
-  colleges!: Observable<any[]>;
-  programs!: Observable<any[]>;
-
   userName$!: Observable<string>;
   displayNameFirstHalf: string = '';
+  currentID: string = '';
+  userID$!: Observable<string>;
+
+  EnrolledSubjects: Subject[] = [];
 
   constructor(public authService: AuthService, private afAuth: AngularFireAuth, private afs: AngularFirestore, private router: Router) {}
 
-  fetchColleges(): Observable<any[]> {
-    return this.afs.collection('Colleges').snapshotChanges().pipe(
-      map((colleges: any[]) => {
-        return colleges.map(college => {
-          const data = college.payload.doc.data() as CollegeData;
-          return {
-            value: college.payload.doc.id,
-            label: data.CollegeName
-          };
-        });
-      })
-    );
-  }
+  ngOnInit(): void {
+    this.userName$ = this.authService.getCurrentUserName();
+    this.userID$ = this.authService.getUid();
 
-  fetchPrograms(collegeDocument: string): Observable<any[]> {
-    console.log('College Document:', collegeDocument);
-
-    const collegeRef = this.afs.collection('Colleges').doc(collegeDocument).ref;
-
-    return this.afs.collection('Programs', ref =>
-      ref.where('ProgCollege', '==', collegeRef)
-    ).snapshotChanges().pipe(
-      map(programs => {
-        return programs.map(program => {
-          const data = program.payload.doc.data() as ProgramData;
-          return {
-            value: program.payload.doc.id,
-            label: data.ProgName
-          };
-        });
+    this.userID$.pipe(
+      switchMap(uid => {
+        return this.afs.collection('users').doc(uid).valueChanges();
       }),
       catchError(error => {
-        console.error('Error fetching programs:', error);
+        console.error('Error fetching user data:', error);
         return [];
-      })
-    );
-  }
+      }),
+      map((userDoc: any) => userDoc.enrolledTo || [])
+    ).subscribe(enrolledSubjects => {
+      console.log('Enrolled Subjects:', enrolledSubjects);
 
-  onCollegeSelected(): void {
-    console.log('onCollegeSelected called');
-    this.programs = this.fetchPrograms(this.college);
-  }
-
-  ngOnInit(): void {
-    this.colleges = this.fetchColleges();
-    this.userName$ = this.authService.getCurrentUserName();
-
-    this.userName$.subscribe(displayName => {
-      if (displayName !== 'User') {
-        const displayNameParts = displayName.split(' ');
-        if (displayNameParts.length > 0) {
-          this.displayNameFirstHalf = displayNameParts.slice(0, Math.ceil(displayNameParts.length / 2)).join(' ');
-        }
-      }
+      forkJoin<Subject[]>(
+        enrolledSubjects.map((subjectCode: any) =>
+          this.afs.collection('Subjects', ref => ref.where('code', '==', subjectCode)).valueChanges() as Observable<Subject[]>
+        )
+      ).subscribe(subjectsArrays => {
+        this.EnrolledSubjects = subjectsArrays.flat();
+      });
     });
   }
+
 }
